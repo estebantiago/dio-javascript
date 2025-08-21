83cafb748e72d77e2b959e3aa30d4676fe336bdf
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-21 13:21:55 -0300

    estudando arrays

---
+++ aulas/arrays/aula.js
@@ -0,0 +1,21 @@
+let notas = [];
+
+
+
+notas.push(10);
+notas.push(5);
+notas.push(7);
+notas.push(4);
+notas.push(5);
+
+
+let soma = 0;
+
+for(let i = 0; i < notas.length; i++){
+       const nota = notas[i];
+       soma = soma + nota;
+}
+
+let media = soma / notas.length;
+
+console.log(media)
