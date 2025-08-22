1ea8547621bae3d58a29e8b8b3eddbfeb4d25be1
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-22 10:52:57 -0300

    estudando importações

---
+++ importacao/funcoes-auxiliares.js
@@ -0,0 +1,29 @@
+/*
+Uma sala contém 5 alunos, e para cada aluno foi sorteado um número de 1 a 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+Dados de entrada : 
+5
+50
+10
+98
+23
+
+Saida :
+
+98
+
+*/
+
+
+function gets(){
+    return 10;
+}
+function print(teste){
+    console.log('teste');
+}   
+
+module.exports = {
+    gets,
+    print
+}
