a5a14612d6b7824609458d8d7c4f7163b6814f77
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-27 20:54:15 -0300

    atualizando ex02

--- importacao/exercicios/exercicio2.js
+++ importacao/exercicios/exercicio2.js
@@ -1,7 +1,25 @@
-const {gets, print} = require('./funcoes-auxiliares2');
+const { gets, print } = require('./funcoes-auxiliares2');
+
+const n = gets();
+let maiorNumPar = 0;
+let menorNumImpar = 0;
 
 
-
-for(let i = 0; i < entradas.lenght; i++){
-       
+for (let i = 0; i < n; i++) {
+    const numero = gets();
+    if (numero % 2 === 0) {
+        if (numero > maiorNumPar) {
+            maiorNumPar = numero;
+        } else {
+            if (menorNumImpar === 0) {
+                menorNumImpar = numero;
+            } else if (numero < menorNumImpar) {
+                menorNumImpar = numero;
+            }
+        }
+    }
 }
+print(maiorNumPar);
+print(menorNumImpar);
+
+
