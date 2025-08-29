be1d4c2b7b33fdbb6b9a0425a074b6f78c4556bc
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-29 08:30:47 -0300

    atualizando ex02

--- importacao/exercicios/exercicio2.js
+++ importacao/exercicios/exercicio2.js
@@ -1,25 +1,26 @@
 const { gets, print } = require('./funcoes-auxiliares2');
 
-const n = gets();
-let maiorNumPar = 0;
-let menorNumImpar = 0;
+const n = gets(); // atribuindo gets() a variavel N
 
+let maiorNumPar = null; // Maior número par inicia com 0
+let menorNumImpar = null; // Menor número impar inicia com 0 
 
-for (let i = 0; i < n; i++) {
-    const numero = gets();
-    if (numero % 2 === 0) {
-        if (numero > maiorNumPar) {
-            maiorNumPar = numero;
-        } else {
-            if (menorNumImpar === 0) {
-                menorNumImpar = numero;
-            } else if (numero < menorNumImpar) {
-                menorNumImpar = numero;
-            }
+for(let i = 0; i < n; i++){ // enquanto i recebe 0; i for menor que n(gets()); i incrementa
+
+    const numero = gets(); // numero recebe gets()
+
+    if(numero % 2 === 0){ // se o número(gets()) for par 
+
+        if(maiorNumPar === null || numero > maiorNumPar){ // Se numero(gets()) for maior que o maior numero par 
+            maiorNumPar = numero; // Maior numero par recebe numero(gets())
+        } 
+    } else { // Se não 
+        if( menorNumImpar === null || numero < menorNumImpar){ // Se o menor número impar for igual a 0
+            menorNumImpar = numero; // O menor número impar recebe numero(gets())
         }
+
     }
-}
-print(maiorNumPar);
-print(menorNumImpar);
+}   
 
-
+print(`Maior número par: ${maiorNumPar}`);
+print(`Menor número Impar: ${menorNumImpar}`);
